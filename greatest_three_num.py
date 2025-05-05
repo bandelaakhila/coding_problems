17f4b500f5811ca11c67235c4ce910d0ac839d0d
bandelaakhila <bandelaakhila12@gmail.com>
2025-05-05 14:14:50 +0530

    Add files via upload

---
+++ greatest_three_num.py
@@ -0,0 +1,10 @@
+#greatest of three numbers
+num1=int(input())
+num2=int(input())
+num3=int(input())
+if num1>num2:
+    print("the greatest num is : ",num1)
+elif num2>num3:
+    print("the greatest num is: ", num2)
+else:
+    print("the greatest num is: ",num3)
